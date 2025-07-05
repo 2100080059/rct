8d62deaa8b9f5aa98c86bf57e58a61381e6bde57
vignesh <125268371+2100080059@users.noreply.github.com>
2025-07-05 10:21:03 +0530

    Update updateDate.js

--- assets/js/updateDate.js
+++ assets/js/updateDate.js
@@ -2,7 +2,7 @@
 function formatIndianNumber(num) {
   return num.toString()
     .replace(/\B(?=(\d{2})+(?!\d{3}))/g, ",")
-    .replace(/(\d+)(?=(\d{3})+(?!\d))/g, "$$1,");
+    .replace(/(\d+)(?=(\d{3})+(?!\d))/g, "$1,");
 }
 
 // Visitor count using localStorage
