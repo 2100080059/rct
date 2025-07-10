808dcc5470338f50e4842fee3549b331b8adf93e
vignesh <125268371+2100080059@users.noreply.github.com>
2025-07-10 20:14:16 +0530

    Create firebase-loader.js

---
+++ assets/js/firebase-loader.js
@@ -0,0 +1,3 @@
+document.write('<script src="https://www.gstatic.com/firebasejs/9.24.0/firebase-app-compat.js"><\/script>');
+document.write('<script src="https://www.gstatic.com/firebasejs/9.24.0/firebase-database-compat.js"><\/script>');
+document.write('<script src="/js/visitor-counter.js"><\/script>');
